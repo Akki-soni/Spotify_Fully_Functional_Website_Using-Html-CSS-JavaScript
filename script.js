311ea7839c72cf2cf269e939463122e6fa9afa7e
AKKI-SONI <av6444757@gmail.com>
2025-07-29 11:42:17 +0530

    All Projects Files

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`/${folder}/`); // (http://127.0.0.1:5500)
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -78,7 +78,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-  let a = await fetch(`/songs/`); // (http://127.0.0.1:5500)
+  let a = await fetch(`/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -88,10 +88,10 @@ async function displayAlbums() {
   for (let index = 0; index < array.length; index++) {
     const e = array[index];
 
-    if (e.href.includes("/songs/")) {
+    if (e.href.includes("/songs/") && !e.href.includes(".htaccess")) {
       let folder = e.href.split("/").slice(-2).join("/");
       // Get the Metadata of the folder
-      let a = await fetch(`/${folder}/info.json`); // (http://127.0.0.1:5500)
+      let a = await fetch(`/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML =
         cardContainer.innerHTML +
@@ -185,6 +185,11 @@ async function main() {
     .getElementsByTagName("input")[0]
     .addEventListener("change", (e) => {
       currentSong.volume = parseInt(e.target.value) / 100;
+      if (currentSong.volume > 0) {
+        document.querySelector(".volume>img").src = document
+          .querySelector(".volume>img")
+          .src.replace("images/mute.svg", "images/volume.svg");
+      }
     });
 
   // Add Event Listener to Mute the Track
